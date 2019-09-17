4f55e4dc6643dd5b8e3958f6f3996850dc11b1a8
zengsheng <roman.zengsheng@gmail.com>
2019-09-17 21:52:02 +0800

    NKN wallet should make rpcAddr works

--- config/NKN/accountInfoNKN.js
+++ config/NKN/accountInfoNKN.js
@@ -3,7 +3,20 @@ let nknWallet = require("nkn-wallet");
 let keystoreContent = require("./keystoreContent.json");
 let keystoreContentStr = JSON.stringify(keystoreContent);
 let password = require("./password.json").password;
+
+
+let rpcAddr = null;
+if(process.env.NODE_ENV === "production") {
+  rpcAddr = "https://mainnet-rpc-node-0001.nkn.org/mainnet/api/wallet";
+} else if(process.env.NODE_ENV === "development") {
+  rpcAddr = "https://mainnet-rpc-node-0001.nkn.org/mainnet/api/wallet";
+} else {
+  rpcAddr = "https://mainnet-rpc-node-0001.nkn.org/mainnet/api/wallet";
+}
+
+nknWallet.configure({rpcAddr: rpcAddr});
 let instanceNKNWallet = null;
+
 try {
   instanceNKNWallet = nknWallet.loadJsonWallet(keystoreContentStr, password);
   console.log(`NKN wallet ${instanceNKNWallet.address} decryption success!`);
